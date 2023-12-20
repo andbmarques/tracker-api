fa4029c828369ca9e7be46967b08f41a1b3ce248
Anderson Barbosa <andbmarques@gmail.com>
2023-12-19 21:37:15 -0300

    :wrench: Fix | CORS Origins

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const authRoute = require("./src/routes/auth.route");
 db();
 app.use(express.json());
 app.use(cors({
-    origin: "https://tracker-opal-alpha.vercel.app"
+    origin: ["https://tracker-opal-alpha.vercel.app", "http://localhost:5173"]
 }));
 app.use("/user", userRoute);
 app.use("/transactions", transactionsRoute);
