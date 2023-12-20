1447dd7af01d140b63e0f1f38d1be89dace2f9a7
Anderson Barbosa <andbmarques@gmail.com>
2023-12-19 21:08:21 -0300

    :wrench: Fix | Corrigindo a url da origem no CORS

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const authRoute = require("./src/routes/auth.route");
 db();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://tracker-opal-alpha.vercel.app"
 }));
 app.use("/user", userRoute);
 app.use("/transactions", transactionsRoute);
